0b4d9093ef719b7fd3880410e8ae5acd7a33b581
michael.hart.au <michael.hart.au@b2abb7dc-434b-0410-88ff-0187f9076f30>
2008-07-03 13:52:07 +0000

    Rolled back Ninject DLL to version 1.0 Added tests project and supporting libraries

---
+++ trunk/Interception/NinjectContrib.Interception.Tests/Mocks/MockModule.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Core;
+
+namespace NinjectContrib.Interception.Tests.Mocks
+{
+    class MockModule : StandardModule
+    {
+        public override void Load()
+        {
+            Bind<MockObject>().ToSelf().WithArgument("myProperty", "start");
+        }
+    }
+}
---
+++ trunk/Interception/NinjectContrib.Interception.Tests/PropertyFixture.cs
@@ -0,0 +1,154 @@
+﻿using Ninject.Core;
+using Ninject.Integration.LinFu;
+using NinjectContrib.Interception.Tests.Mocks;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace NinjectContrib.Interception.Tests
+{
+    [TestFixture]
+    public class PropertyFixture
+    {
+        [Test]
+        public void PropertyGetInterceptedWithReplace()
+        {
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule()))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.GetMyProperty(), Is.EqualTo("start"));
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+            }
+
+            var testModule = new InlineModule(
+                m => m.InterceptReplaceGet<MockObject>(o => o.MyProperty, i => i.ReturnValue = "intercepted")
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+                
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.GetMyProperty(), Is.EqualTo("start"));
+                Assert.That(obj.MyProperty, Is.EqualTo("intercepted"));
+            }
+        }
+
+        [Test]
+        public void PropertySetInterceptedWithReplace()
+        {
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule()))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                
+                obj.MyProperty = "end";
+
+                Assert.That(obj.MyProperty, Is.EqualTo("end"));
+            }
+
+            var testModule = new InlineModule(
+                m => m.InterceptReplaceSet<MockObject>(o => o.MyProperty, i => {})
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+
+                obj.MyProperty = "end";
+
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+            }
+        }
+
+        [Test]
+        public void PropertyGetInterceptedBefore()
+        {
+            var testString = "empty";
+
+            var testModule = new InlineModule(
+                m => m.InterceptBeforeGet<MockObject>(o => o.MyProperty, i => { if (i.ReturnValue == null) { testString = "null"; } })
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+                
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(testString, Is.EqualTo("empty"));
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                Assert.That(testString, Is.EqualTo("null"));
+            }
+        }
+
+        [Test]
+        public void PropertySetInterceptedBefore()
+        {
+            var testModule = new InlineModule(
+                m => m.InterceptBeforeSet<MockObject>(o => o.MyProperty, i => i.Request.Arguments[0] = "intercepted")
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                
+                obj.MyProperty = "end";
+                
+                Assert.That(obj.MyProperty, Is.EqualTo("intercepted"));
+            }
+        }
+
+        [Test]
+        public void PropertyGetInterceptedAfter()
+        {
+            var testString = "empty";
+            
+            var testModule = new InlineModule(
+                m => m.InterceptAfterGet<MockObject>(o => o.MyProperty, i => testString = i.ReturnValue.ToString())
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(testString, Is.EqualTo("empty"));
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                Assert.That(testString, Is.EqualTo("start"));
+            }
+        }
+
+        [Test]
+        public void PropertySetInterceptedAfter()
+        {
+            var testString = "empty";
+            
+            var testModule = new InlineModule(
+                m => m.InterceptAfterSet<MockObject>(o => o.MyProperty, i => testString = ((MockObject)i.Request.Target).MyProperty)
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(testString, Is.EqualTo("empty"));
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+
+                obj.MyProperty = "end";
+
+                Assert.That(obj.MyProperty, Is.EqualTo("end"));
+                Assert.That(testString, Is.EqualTo("end"));
+            }
+        }
+    }
+}
