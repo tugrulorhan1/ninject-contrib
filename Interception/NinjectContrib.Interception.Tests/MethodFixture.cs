34977ea85795b8919c61fccc434e069c0d238850
michael.hart.au <michael.hart.au@b2abb7dc-434b-0410-88ff-0187f9076f30>
2008-07-03 13:52:07 +0000

    Rolled back Ninject DLL to version 1.0 Added tests project and supporting libraries

---
+++ Interception/NinjectContrib.Interception.Tests/MethodFixture.cs
@@ -0,0 +1,86 @@
+﻿using Ninject.Core;
+using Ninject.Integration.LinFu;
+using NinjectContrib.Interception.Tests.Mocks;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace NinjectContrib.Interception.Tests
+{
+    [TestFixture]
+    public class MethodFixture
+    {
+        [Test]
+        public void MethodInterceptedWithReplace()
+        {
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule()))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                Assert.That(obj.GetMyProperty(), Is.EqualTo("start"));
+            }
+
+            var testModule = new InlineModule(
+                m => m.InterceptReplace<MockObject>(o => o.GetMyProperty(), i => i.ReturnValue = "intercepted")
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+                
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                Assert.That(obj.GetMyProperty(), Is.EqualTo("intercepted"));
+            }
+        }
+
+        [Test]
+        public void MethodInterceptedBefore()
+        {
+            var testString = "empty";
+            
+            var testModule = new InlineModule(
+                m => m.InterceptBefore<MockObject>(o => o.SetMyProperty(""), i => testString = ((MockObject)i.Request.Target).MyProperty)
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+                
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                Assert.That(testString, Is.EqualTo("empty"));
+                
+                obj.SetMyProperty("end");
+                
+                Assert.That(obj.MyProperty, Is.EqualTo("end"));
+                Assert.That(testString, Is.EqualTo("start"));
+            }
+        }
+
+        [Test]
+        public void MethodInterceptedAfter()
+        {
+            var testString = "empty";
+            
+            var testModule = new InlineModule(
+                m => m.InterceptAfter<MockObject>(o => o.SetMyProperty(""), i => testString = ((MockObject)i.Request.Target).MyProperty)
+            );
+
+            using (var kernel = new StandardKernel(new LinFuModule(), new MethodInterceptorModule(), new MockModule(), testModule))
+            {
+                var obj = kernel.Get<MockObject>();
+
+                Assert.That(obj, Is.Not.Null);
+                Assert.That(obj.MyProperty, Is.EqualTo("start"));
+                Assert.That(testString, Is.EqualTo("empty"));
+
+                obj.SetMyProperty("end");
+
+                Assert.That(obj.MyProperty, Is.EqualTo("end"));
+                Assert.That(testString, Is.EqualTo("end"));
+            }
+        }
+    }
+}
---
+++ Interception/NinjectContrib.Interception.Tests/Mocks/MockObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NinjectContrib.Interception.Tests.Mocks
+{
+    public class MockObject
+    {
+        public virtual string MyProperty { get; set; }
+
+        public MockObject(string myProperty)
+        {
+            MyProperty = myProperty;
+        }
+
+        public virtual string GetMyProperty()
+        {
+            return MyProperty;
+        }
+
+        public virtual void SetMyProperty(string myProperty)
+        {
+            MyProperty = myProperty;
+        }
+    }
+}
